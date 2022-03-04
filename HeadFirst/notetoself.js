832196fdc2d69bdc6b41f870da37b008c706b66d
SidewardsBanana <Jlxfallon@gmail.com>
2022-03-04 16:40:51 -0500

    New method... Broken

--- HeadFirst/notetoself.js
+++ HeadFirst/notetoself.js
@@ -1,39 +1,34 @@
-window.onload = init;
+function getStickiesArray() {
+    var stickiesArray = localStorage.getItem("stickiesArray");
+    if (!stickiesArray) {
+        stickiesArray = [];
+        localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+} else {
+    stickiesArray = JSON.parse(stickiesArray);
+}
+return stickiesArray;
+}
 
 function init() {
-    var button = document.getElementById("addButton");
+    var button = document.getElementById("add_button");
     button.onclick = createSticky;
 
-    for (var i = 0; i < localStorage.length; i++){
-        var key = localStorage.key(i);
-        if (key.substring(0, 6) == "sticky") {
-            var value = localStorage.getItem(key);
-            addStickyToDOM(value);
+    var stickiesArray = getStickiesArray();
+
+    for (var i = 0; i < stickiesArray.length; i++) {
+        var key = stickiesArray[i];
+        var value = localStorage[key];
+        addStickyToDOM(value);
         }
     }
-}
-
-function addStickyToDOM(value) {
-    var stickies = document.getElementById("stickies");
-    var sticky = document.createElement("li");
-    var span = document.createElement("span");
-    span.setAttribute("class", "sticky");
-    span.innerHTML = value;
-    sticky.appendChild(span);
-    stickies.appendChild(sticky);
-}
 
 function createSticky() {
-    var value = document.getElementById("noteText").value;
-    var key = "sticky_" + localStorage.length;
+    var stickiesArray = getStickiesArray();
+    var currentDate = new Date();
+    var key = "sticky_" + currentDate.getTime();
+    var value = document.getElementById("note_text").value;
     localStorage.setItem(key, value);
-
+    stickiesArray.push(key);
+    localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
     addStickyToDOM(value);
 }
-
-localStorage.setItem("sticky_0", "Take Shower");
-localStorage.setItem("sticky_1", "Get Dressed");
-
-var sticky = localStorage.getItem("sticky_0");
-
-alert(sticky);
