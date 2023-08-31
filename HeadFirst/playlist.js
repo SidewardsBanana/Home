a1990bdc22f00e0f31ae20d4a222eb7e72626d63
SidewardsBanana <jlxfallon@gmail.com>
2023-08-31 02:37:45 -0400

    Merge and Fix?

---
+++ HeadFirst/playlist.js
@@ -0,0 +1,22 @@
+window.onload = init;
+function init() {
+let button = document.getElementById("addButton")
+button.onclick = handleButtonClick;
+}
+
+function init() {
+    let button = document.getElementById("addButton");
+    button.onclick = handleButtonClick;
+    loadPlaylist();
+}
+
+function handleButtonClick() {
+    let textInput = document.getElementById("songTextInput");
+    let songName = textInput.value;
+    let li = document.createElement("li");
+    li.innerHTML = songName;
+    let ul = document.getElementById("playlist");
+    ul.appendChild(li);
+    save(songName);
+}
+
